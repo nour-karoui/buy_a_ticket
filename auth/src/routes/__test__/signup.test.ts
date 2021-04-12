921ccdcca994d3148669a02847874fcef101433d
nour-karoui <nkaroui1998@gmail.com>
2021-04-12 10:02:14 +0100

    change expectation

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -8,7 +8,7 @@ it('returns a 201 on successful signup', async () => {
             email: 'test@test.com',
             password: 'password'
         })
-        .expect(400);
+        .expect(201);
 });
 
 it('returns a 400 with an invalid email', async () => {
